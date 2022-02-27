b88bac99c40a98d56bea6533799227f6fa6bdb54
Oscar Ligthart <oscarligthart@gmail.com>
2022-02-27 13:47:36 +0100

    created project structure

---
+++ super_mario_sac/test.py
@@ -0,0 +1,12 @@
+#
+# File: super_mario_sac/test.py
+# Auth: Oscar Ligthart
+# Desc: The test script for the Super Mario agent
+#
+####################################################
+
+
+def test():
+    # todo implement
+    return
+
---
+++ super_mario_sac/train.py
@@ -0,0 +1,12 @@
+#
+# File: super_mario_sac/train.py
+# Auth: Oscar Ligthart
+# Desc: The train script for the Super Mario agent
+#
+####################################################
+
+
+def train():
+    # todo implement
+    return
+
